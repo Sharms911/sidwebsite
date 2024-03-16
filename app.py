380ccfafae266295bada6213a120afddd898dd7a
Sharms911 <132896436+Sharms911@users.noreply.github.com>
2024-03-16 12:09:50 +1030

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('contact')
+@app.route('/contact')
 def contact():
-    return render_template('/contact.html')
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
