3eba9919367d47d32e22d88bdd0e8edc6622374e
Sid Sharma <132896436+Sharms911@users.noreply.github.com>
2024-06-01 11:27:14 +0930

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,5 @@ app.config['STATIC_URL_PATH'] = '/sidwebsite/static'
 def index():
     return render_template('index.html')
 
-@app.route('/sidwebsite/contact')
-def contact():
-    return render_template('contact.html')
-
 if __name__ == "__main__":
     app.run(debug=True)
